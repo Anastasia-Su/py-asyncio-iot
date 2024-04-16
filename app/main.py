85f02c3965f2fcb346c9ca93dd8cc8de443b0d7b
Anastasia <anastasiiya.p@gmail.com>
2024-04-16 16:45:41 +0300

    Add sequence and parallel processing

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 import asyncio
 import time
+from typing import Awaitable, Any
 
 from iot.devices import HueLightDevice, SmartSpeakerDevice, SmartToiletDevice
 from iot.message import Message, MessageType
@@ -7,42 +8,52 @@ from iot.service import IOTService
 
 
 async def main() -> None:
-    # create an IOT service
     service = IOTService()
 
-    # create and register a few devices
     hue_light = HueLightDevice()
     speaker = SmartSpeakerDevice()
     toilet = SmartToiletDevice()
-    hue_light_task = asyncio.create_task(service.register_device(hue_light))
-    speaker_task = asyncio.create_task(service.register_device(speaker))
-    toilet_task = asyncio.create_task(service.register_device(toilet))
 
-    hue_light_id = await hue_light_task
-    speaker_id = await speaker_task
-    toilet_id = await toilet_task
+    async def run_sequence(*functions: Awaitable[Any]) -> None:
+        for function in functions:
+            await function
 
+    async def run_parallel(
+            *functions: Awaitable[Any]
+    ) -> tuple[Awaitable[Any]]:
+        return await asyncio.gather(*functions)
 
-    # create a few programs
+    [hue_light_id, speaker_id, toilet_id] = await run_parallel(
+        service.register_device(hue_light),
+        service.register_device(speaker),
+        service.register_device(toilet)
+    )
+
     wake_up_program = [
         Message(hue_light_id, MessageType.SWITCH_ON),
         Message(speaker_id, MessageType.SWITCH_ON),
-        Message(speaker_id, MessageType.PLAY_SONG, "Rick Astley - Never Gonna Give You Up"),
+    ]
+
+    actions_program = [
+        Message(
+            speaker_id,
+            MessageType.PLAY_SONG,
+            "Rick Astley - Never Gonna Give You Up"
+        ),
+        Message(toilet_id, MessageType.FLUSH),
     ]
 
     sleep_program = [
         Message(hue_light_id, MessageType.SWITCH_OFF),
         Message(speaker_id, MessageType.SWITCH_OFF),
-        Message(toilet_id, MessageType.FLUSH),
         Message(toilet_id, MessageType.CLEAN),
     ]
 
-    # run the programs
-    wake_up_task = asyncio.create_task(service.run_program(wake_up_program))
-    sleep_task = asyncio.create_task(service.run_program(sleep_program))
-
-    await wake_up_task
-    await sleep_task
+    await run_sequence(
+        run_parallel(service.run_program(wake_up_program)),
+        run_parallel(service.run_program(actions_program)),
+        run_parallel(service.run_program(sleep_program))
+    )
 
 
 if __name__ == "__main__":
